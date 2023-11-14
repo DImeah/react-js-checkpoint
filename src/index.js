7d595e7360f5ab778820a9948ed9e453bf3462e4
Imeah <imeahdornu@gmail.com>
2023-11-14 13:33:10 +0100

    fixed bootstrap installation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
