5076656f5353148d2d6eb55df8420d1fc2eed2ec
Imeah <imeahdornu@gmail.com>
2023-11-14 12:48:48 +0100

    changed the  form

--- src/components/form-section/Form.js
+++ src/components/form-section/Form.js
@@ -1,21 +1,31 @@
 import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 
 function form() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Enter Your Name</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-    </Form>
+    <Container>
+      <div className="container mb-5 mt-5">
+        <Container>
+          <h2 className="display-4 mt-3 text-center">Get in Touch</h2>
+        </Container>
+        <Form className="container m-3t form-area">
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            {/* <Form.Label className="form-control">Your Name</Form.Label> */}
+            <Form.Control type="email" placeholder="Enter Your Name" />
+            <Form.Control type="email" placeholder="Email" />
+            <Form.Label></Form.Label>
+            <Form.Control as="textarea" rows={3} />
+            <input
+              class="btn btn-primary mt-3"
+              type="submit"
+              value="Submit"
+              placeholder="Please type your message here"
+            ></input>
+          </Form.Group>
+        </Form>
+      </div>
+    </Container>
   );
 }
 
